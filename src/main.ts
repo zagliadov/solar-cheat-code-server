4a3a2acda8bd8522bdb687078a663b12d3ad85b5
Daniil <portal.sid.grand2@gmail.com>
2024-05-19 11:00:01 +0300

    env PORT

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
